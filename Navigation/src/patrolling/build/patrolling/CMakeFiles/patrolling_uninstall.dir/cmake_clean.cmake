8f8827b3a8f20f22f4a68677900e15e568a86a16
jordialt <al416445@uji.es>
2023-12-04 15:46:03 +0100

    Corregido error al hacer colcon build desde otra máquina

--- Navigation/src/patrolling/build/patrolling/CMakeFiles/patrolling_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/patrolling_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/patrolling_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
